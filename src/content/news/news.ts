ee6832a9772eac2775687da3c3a936b65f6bf7b5
miguelag99 <miguelag99@hotmail.com>
2024-11-22 23:46:50 +0100

    Publications list first version

--- src/content/news/news.ts
+++ src/content/news/news.ts
@@ -5,8 +5,8 @@ export const news = [
       {
         date: 'September',
         title: 'ITSC 2024',
-        description: 'Our research paper **Fast and Efficient Transformer-based Method for Birds Eye View Instance Prediction** was presented at 27th IEEE International Conference on Intelligent Transportation Systems.',
-        link: '#',
+        description: 'Our research paper <strong>Fast and Efficient Transformer-based Method for Birds Eye View Instance Prediction</strong> was presented at 27th IEEE International Conference on Intelligent Transportation Systems.',
+        link: './publications/ITSC_24_FastAndEfficient',
         icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline ml-2 hover:text-accent transition-colors">
           <path d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"/>
         </svg>`
@@ -18,13 +18,21 @@ export const news = [
     items: [
       {
         date: 'November',
-        title: 'Research Grant Awarded',
-        description: 'Received funding for innovative research project from <strong>[Funding Organization]</strong>.',
+        title: 'Iberian Robotics Conference',
+        description: 'We presented our research paper <strong>Exploring Domain Adaptation with Depth-Based 3D Object Detection in CARLA Simulator</strong> at the 6th Iberian Robotics Conference.',
+        link: './publications/ROBOT23',
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline ml-2 hover:text-accent transition-colors">
+          <path d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"/>
+        </svg>`
       },
       {
-        date: 'October 2023',
-        title: 'Industry Collaboration Initiated',
-        description: 'Began a collaborative research project with <strong>[Company Name]</strong>.',
+        date: 'September',
+        title: 'Master\'s Final Thesis',
+        description: 'My Master\'s Final Thesis with <strong>Study of camera-based systems for three-dimensional object detection in autonomous driving</strong> was presented at with honours.',
+        link: 'https://ebuah.uah.es/dspace/handle/10017/58112',
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline ml-2 hover:text-accent transition-colors">
+          <path d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"/>
+        </svg>`
       }
     ]
   }
