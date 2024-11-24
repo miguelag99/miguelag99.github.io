b9c9fe952ae84596d2259ae73d8240601e973d99
miguelag99 <miguelag99@hotmail.com>
2024-11-24 18:41:55 +0100

    New info and CV

--- src/content/news.ts
+++ src/content/news.ts
@@ -18,7 +18,7 @@ export const news = [
     items: [
       {
         date: 'November',
-        title: 'Iberian Robotics Conference',
+        title: 'Sixth Iberian Robotics Conference',
         description: 'We presented our research paper <strong>Exploring Domain Adaptation with Depth-Based 3D Object Detection in CARLA Simulator</strong> at the 6th Iberian Robotics Conference.',
         link: './publications/ROBOT23',
         icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline ml-2 hover:text-accent transition-colors">
